86737f6ac50b9662e331eb6d494f4537509acc8c
pmzone31 <pmzone31@gmail.com>
2025-08-19 18:01:26 +0000

    Update AppForm.jsx

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -17,7 +17,7 @@ export default function AppForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/submit', {
+      const response = await fetch('https://qargui.azurewebsites.net/api/http_trigger1?code=fiXhwjKAYqpShMnsDRRdO4WfUCWn8f8Xx-b5lHfIzJhjAzFuMgApHA==', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
